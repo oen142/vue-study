b60e239f0810a7dd0febb6bef97d8c5036f35e23
김종완 <oen142@naver.com>
2020-08-24 22:43:14 +0900

    vue 초급 끝

---
+++ learn-vue-js/vue-form/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <form v-on:submit="submitForm">
+            <div>
+                <label for="username">id :</label>
+                <input id="username" type="text" v-model="username">
+            </div>
+            <div>
+                <label for="password">password :</label>
+                <input id="password" type="password" v-model="password">
+            </div>
+            <button type="submit" >login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "App",
+        data: function(){
+            return {
+                username : '',
+                password : '',
+            }
+        },
+        methods :{
+            submitForm: function(){
+                event.preventDefault();
+                console.log(this.username, this.password);
+
+                let url = 'https://jsonplaceholder.typicode.com/users';
+                let data = {
+                    username: this.username,
+                    password: this.password
+                }
+                axios.post(url , data)
+                    .then(function(response){
+                        console.log(response);
+                    })
+                    .catch(function(error){
+                        console.log(error)
+                    });
+
+            }
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
