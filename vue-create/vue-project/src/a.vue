b30d5da10d00acf4fea380f01635b08c548d70a5
wani <oen142@naver.com>
2020-08-24 19:25:23 +0900

    vue init

---
+++ vue-create/vue-project/src/a.vue
@@ -0,0 +1,46 @@
+<!--
+한파일에서 관리하겠다.
+-->
+
+<!--
+var appHeader ={
+    template : '<div>header</div>',
+    methods: {
+
+        addNum: function(){
+        }
+
+    }
+
+}
+-->
+
+<template>
+   <!--
+   html
+   -->
+    <div>header</div>
+</template>
+
+<script>
+    /*
+    * javascript 인스턴스 옵션
+    * */
+    export default {
+
+        name: "a",
+        methods : {
+            addNum: function(){
+
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+    /*
+    css
+    */
+</style>
